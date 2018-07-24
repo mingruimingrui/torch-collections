7857e45d4ef3887483316aa03f2f99bc06bc0ca8
mingruimingrui <mingruimingrui@hotmail.com>
2018-07-24 11:46:04 +0800

    added simple setup script

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='torch-collections',
+    version='1.0',
+    description='A collection of deep learning models, and utility tookit written in torch',
+    url='https://github.com/mingruimingrui/torch-pipeline',
+    author='Wang Ming Rui',
+    author_email='mingruimingrui@hotmail.com',
+    packages=[
+        'torch_collections',
+        'torch_collections.losses',
+        'torch_collections.models',
+        'torch_collections.modules',
+        'torch_collections.utils'
+    ]
+)
