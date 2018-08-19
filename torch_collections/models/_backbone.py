ddbb24abbaafaa400a3d6a72e3caa2413f033870
mingruimingrui <mingruimingrui@hotmail.com>
2018-08-19 23:26:10 +0800

    added squeezenet into backbone

--- torch_collections/models/_backbone.py
+++ torch_collections/models/_backbone.py
@@ -3,11 +3,12 @@ will be no FPN class. Instead there will be functions which will build a FPN
 based on the backbone_name (along with getters for other neccessary backbone
 specific variables and functions).
 """
-# TODO: Implement squeeze net
 
 def build_backbone_model(backbone_name, freeze_backbone=False):
     if 'resnet' in backbone_name:
         from ._resnet import ResNetBackbone as Backbone
+    elif 'squeezenet' in backbone_name:
+        from ._squeezenet import SqueezeNetBackbone as Backbone
     else:
         raise Exception('{} has not been implemented yet'.format(backbone_name))
 
@@ -17,6 +18,9 @@ def get_backbone_channel_sizes(backbone_name):
     if 'resnet' in backbone_name:
         from ._resnet import get_resnet_backbone_channel_sizes
         backbone_channel_sizes = get_resnet_backbone_channel_sizes(backbone_name)
+    elif 'squeezenet' in backbone_name:
+        from ._squeezenet import get_squeezenet_backbone_channel_sizes
+        backbone_channel_sizes = get_squeezenet_backbone_channel_sizes(backbone_name)
     else:
         raise Exception('{} has not been implemented yet'.format(backbone_name))
 
@@ -25,6 +29,10 @@ def get_backbone_channel_sizes(backbone_name):
 def build_fpn_feature_shape_fn(backbone_name):
     if 'resnet' in backbone_name:
         from ._resnet import resnet_fpn_feature_shape_fn as fpn_feature_shape_fn
+    elif backbone_name == 'squeezenet1_0':
+        from ._squeezenet import squeezenet1_0_fpn_feature_shape_fn as fpn_feature_shape_fn
+    elif backbone_name == 'squeezenet1_1':
+        from ._squeezenet import squeezenet1_1_fpn_feature_shape_fn as fpn_feature_shape_fn
     else:
         raise Exception('{} has not been implemented yet'.format(backbone_name))
 
